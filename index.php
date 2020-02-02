9a8c88a9a71eb57b23bbf72b5ccfd23e25b9f13b
Rhatcher94 <49662822+Rhatcher94@users.noreply.github.com>
2020-02-01 19:15:32 -0500

    Added description to routing

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 $request = $_SERVER['REQUEST_URI'];
 $req = str_replace("/soupkitchen", "", $request);
 
-
+/* REQUIRES THE .htaccess file */
+/* MAKE SURE YOUR VIEW IS IN THE VIEW FOLDER */
+/* To enclude a route add the route you would like to the case statements */
+/*  Example. to unclude a contact us route. 
+     case '/contact-us':
+        require __DIR__ . '/views/contact_us.php';
+        break;
+*/
 switch ($req) {
     case '/' :
         require __DIR__ . '/views/home.php';
