f41ac1cde90ea475681b1dc4141afaf694ae0f08
Rhatcher94 <49662822+Rhatcher94@users.noreply.github.com>
2020-02-02 09:20:32 -0500

    Routing 2.0

--- Routes/Router.php
+++ Routes/Router.php
@@ -1,20 +1,75 @@
 <?php
-// Grabs the URI and breaks it apart in case we have querystring stuff
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
-// Route it up!
-switch ($request_uri[0]) {
-    // Home page
-    case '/index':
-        require '../views/index.php';
-        break;
-    // About page
-    case '/about':
-        require '../views/about.php';
-        break;
-    // Everything else
-    default:
-        header('HTTP/1.0 404 Not Found');
-        require '../views/404.php';
-        break;
+class Router
+{
+  private $request;
+  private $supportedHttpMethods = array(
+    "GET",
+    "POST"
+  );
+
+  function __construct(IRequest $request)
+  {
+   $this->request = $request;
+  }
+
+  function __call($name, $args)
+  {
+    list($route, $method) = $args;
+
+    if(!in_array(strtoupper($name), $this->supportedHttpMethods))
+    {
+      $this->invalidMethodHandler();
+    }
+
+    $this->{strtolower($name)}[$this->formatRoute($route)] = $method;
+  }
+
+  /**
+   * Removes trailing forward slashes from the right of the route.
+   * @param route (string)
+   */
+  private function formatRoute($route)
+  {
+    $route = str_replace("/SoupKitchen","",$route);
+    $result = rtrim($route, '/');    
+    if ($result === '')
+    {
+      return '/';
+    }
+    return $result;
+  }
+
+  private function invalidMethodHandler()
+  {
+    header("{$this->request->serverProtocol} 405 Method Not Allowed");
+  }
+
+  private function defaultRequestHandler()
+  {
+    header("{$this->request->serverProtocol} 404 Not Found");
+  }
+
+  /**
+   * Resolves a route
+   */
+  function resolve()
+  {
+    $methodDictionary = $this->{strtolower($this->request->requestMethod)};
+    $formatedRoute = $this->formatRoute($this->request->requestUri);
+    $method = $methodDictionary[$formatedRoute];
+
+    if(is_null($method))
+    {
+      $this->defaultRequestHandler();
+      return;
+    }
+
+    echo call_user_func_array($method, array($this->request));
+  }
+
+  function __destruct()
+  {
+    $this->resolve();
+  }
 }
--- index.php
+++ index.php
@@ -1,50 +1,22 @@
 <?php
-include __DIR__.'/Controllers/AccountController.php';
-include __DIR__.'/Controllers/ProductController.php';
 
-$request = $_SERVER['REQUEST_URI'];
+include_once 'Routes/Request.php';
+include_once 'Routes/Router.php';
+$router = new Router(new Request);
 
-//edit this to replace your full path.
-$req = str_replace("/SoupKitchen", "", $request);
-$urlReq = explode('/',$request);
-/* REQUIRES THE .htaccess file */
-/* MAKE SURE YOUR VIEW IS IN THE VIEW FOLDER */
-/* To enclude a route add the route you would like to the case statements */
-/*  Example. to unclude a contact us route. 
-     case '/contact-us':
-        require __DIR__ . '/views/contact_us.php';
-        break;
-*/
-switch ("/".$urlReq[2]) {
-    case '/' :             
-        echo "HOME PAGE";
-        break;    
-    
-    //used for all account requests
-    case '/account' :             
-        $accountController = new AccountController();
-        if($urlReq[3] == "")
-            $accountController->index();
-        elseif($urlReq[3] == "log-out")
-            $accountController->logout();
-        break;    
-   
-    //used for all product requests
-    case '/products' :
-        $productController = new ProductController();
-        if($urlReq[3] == "")
-            $productController->index();
-        elseif($urlReq[3] == "create")
-            $productController->create();
-        elseif($urlReq[3] == "read")
-            $productController->read();
-        elseif($urlReq[3] == "edit")
-            $productController->edit();
-        elseif($urlReq[3] == "destroy")
-            $productController->destroy();
-        break;    
-    default:
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
-}
+$router->get('/', function() {
+  return <<<HTML
+  <h1>Hello world</h1>
+HTML;
+});
+
+$router->get('/products', function($request) {
+  return <<<HTML
+  <h1>Products</h1>
+HTML;
+});
+
+$router->post('/account', function($request) {
+
+  return json_encode($request->getBody());
+});
