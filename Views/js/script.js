9caafbd6ac6542be7b47ff862e7f027c6cbf9af7
jferland09 <55703115+jferland09@users.noreply.github.com>
2020-02-06 23:56:17 -0500

    added event handler for add qty btn

--- Views/js/script.js
+++ Views/js/script.js
@@ -3,54 +3,77 @@ const btnCreateNewRecord = document.querySelector('#create-new-record');
 const btnCloseModal = document.querySelector('#close-modal-create-new-record');
 const btnSaveNewRecord = document.querySelector('#btn-save-new-record');
 const btnAddQty = document.querySelector("#btn-add-qty");
-const APIURL = 'http://localhost:80'
+const APIURL = 'http://localhost:80';
 //import * as apiCalls from './api';
 //  async loadTodos(){
 //     let todos = await apiCalls.getTodos();
 //     this.setState({todos});
 //  }
 const createAnItem = async (newItem = {}) => {
-  //const token = localStorage.token
-  return fetch(APIURL + '/products/create', {
+    //const token = localStorage.token
+    return fetch(APIURL + '/products/create', {
         method: 'post',
         //withCredentials: true,
         //credentials: 'include',
         headers: new Headers({
-        'Content-Type': 'application/json',
-        //'Authorization': 'Bearer ' + token
+            'Content-Type': 'application/json',
+            //'Authorization': 'Bearer ' + token
         }),
         body: JSON.stringify(newItem)
-    })
-    .then(resp => {
-      if(!resp.ok) {
-        if(resp.status >=400 && resp.status < 500) {
-          resp.json().then(data => {
-            return {errorMessage: data.message};
-            // throw err;
-          })
-        } else {
-          return {errorMessage: 'Please try again later, server is not responding'};
-          // throw err;
+    }).then(resp => {
+        if (!resp.ok)
+        {
+            if (resp.status >=400 && resp.status < 500)
+            {
+                resp.json().then(data => {
+                    return {errorMessage: data.message};
+                    // throw err;
+                });
+            }
+            else
+            {
+                return {errorMessage: 'Please try again later, server is not responding'};
+                // throw err;
+            }
         }
-      }
-      return resp.json();
-    })
+        return resp.json();
+    });
 }
 const updateItem = async (newItem = {}) => {
     return fetch(APIURL + "/products/edit", {
-        
-    })
+        mode: "no-cors",
+        method: "post",
+        headers: new Headers ({"Content-Type": "application/json"}),
+        body: JSON.stringify(newItem)
+    }).then(res => {
+        if (!res.ok)
+        {
+            if (res.status >= 400 && res.status < 500)
+            {
+                res.json().then(data => {
+                    return {errorMessage: data.message};
+                    //throw err
+                });
+            }
+            else
+            {
+                return {errorMessage: 'Please try again later, server is not responding'};
+                //throw err
+            }
+        }
+        return res.json();
+    });
 }
 
-btnCreateNewRecord.addEventListener('click', ()=>{
+btnCreateNewRecord.addEventListener('click', () => {
     modal.style.display = 'block';
 });
 
-btnCloseModal.addEventListener('click', ()=>{
+btnCloseModal.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-btnSaveNewRecord.addEventListener('click', async () => {
+btnSaveNewRecord.addEventListener('click', async (req, res) => {
     req = {
         id: document.getElementById('entry-id').value,
         type: document.getElementById('entry-type').value,
@@ -63,5 +86,10 @@ btnSaveNewRecord.addEventListener('click', async () => {
 });
 
 btnAddQty.addEventListener("click", async (req, res) => {
-    
+    var newQty = parseInt(document.getElementById("current-qty").innerHTML);
+    newQty++;
+    req = {qty: newQty};
+    await updateItem(req).then(res => {
+        console.log(res);
+    });
 });
